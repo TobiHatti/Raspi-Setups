55144de73fd4092e0ca218ec5298550c09241be1
Tobias Hattinger <32573581+TobiHatti@users.noreply.github.com>
2020-06-30 17:31:05 +0200

    Create SDLAndCPPInstall.sh

---
+++ Instructions/SDLAndCPPInstall.sh
@@ -0,0 +1,19 @@
+# Install SDL and C++ Compiler (Tested in Ubuntu)
+
+# Step 1) Update APT
+sudo apt-get update
+
+# Step 2) Upgrade APT
+sudo apt-get upgrade
+
+# Step 3) Install C++ Compiler
+sudo apt-get install g++
+
+# Step 4) Install SDL-Modules
+sudo apt-get install libsdl2-2.0
+sudo apt-get install libsdl2-dev
+sudo apt-get install libsdl2-image-dev
+sudo apt-get install libsdl2-ttf-dev
+
+# Done.
+# SDL and C++ can now be run on the raspberry pi
